21fc15e9623edf5925ca5e56f79c7372922968ac
ox <jaimesanchezm@gmail.com>
2024-11-17 15:30:32 +0100

    Flotilla needs to be refloted... but we are missing ssb.db.query handler. Check server, we have it there. However not here (service/browser issue?)

--- src/ssb/flotilla.ts
+++ src/ssb/flotilla.ts
@@ -1,40 +1,37 @@
-import stack from "secret-stack";
 import debugRun from "debug";
 import ssbConfig from "ssb-config";
-
+import SecretStack from 'secret-stack';
 const debug = debugRun("oasis");
 
 const plugins = [
-  // Authentication often hooked for authentication.
-  require("ssb-master"),
-  require("ssb-db"),
-  require("ssb-backlinks"),
-  require("ssb-conn"),
-  require("ssb-about"),
-  require("ssb-blobs"),
-  require("ssb-ebt"),
-  require("ssb-friends"),
-  require("ssb-replication-scheduler"),
-  require("ssb-invite"),
-  require("ssb-lan"),
-  require("ssb-logging"),
-  require("ssb-meme"),
-  require("ssb-no-auth"),
-  require("ssb-onion"),
-  require("ssb-ooo"),
-  require("ssb-plugins"),
-  require("ssb-private1"),
-  require("ssb-query"),
-  require("ssb-room/tunnel/client"),
-  require("ssb-search"),
-  require("ssb-tangle"),
-  require("ssb-unix-socket"),
-  require("ssb-ws"),
+	require("ssb-db2"),
+	require('ssb-db2/compat'),
+	require("ssb-master"),
+	require("ssb-conn"),
+	require("ssb-about"),
+	require("ssb-blobs"),
+	require("ssb-ebt"),
+	require("ssb-friends"),
+	require("ssb-replication-scheduler"),
+	require("ssb-invite"),
+	require("ssb-lan"),
+	require("ssb-logging"),
+	require("ssb-meme"),
+	require("ssb-no-auth"),
+	require("ssb-onion"),
+	require("ssb-plugins"),
+	require("ssb-private1"),
+	require('ssb-db2/full-mentions'),
+	require('ssb-room-client'),
+	require("ssb-search2"),
+	require("ssb-unix-socket"),
+	require("ssb-ws"),
 ];
 
 export default (config) => {
-  const server = stack({});
+  const server = SecretStack( config.caps );
   const walk = (input) => {
+	console.log("wwwwwwwwww--------------------------", server.db)
     if (Array.isArray(input)) {
       input.forEach(walk);
     } else {
@@ -45,5 +42,10 @@ export default (config) => {
 
   walk(plugins);
 
+  console.log("wwwwwwwwww", server.db)
+
+  const c = server({ ...ssbConfig, ...config });
+
+  console.log("wwwwwwwwww", c.db)
   return server({ ...ssbConfig, ...config });
 };
